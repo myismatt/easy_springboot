5fd33c4a6b43284ed1c0be7aa32073e1f2f1fd0b
matt <812262134@qq.com>
2022-07-08 18:21:23 +0800

    update

--- doc/easy_springboot.sql
+++ doc/easy_springboot.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80029
  File Encoding         : 65001
 
- Date: 05/07/2022 18:25:53
+ Date: 08/07/2022 18:21:15
 */
 
 SET NAMES utf8mb4;
@@ -23,18 +23,18 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `easy_config`;
 CREATE TABLE `easy_config`
 (
-    `id`           bigint UNSIGNED                                               NOT NULL AUTO_INCREMENT COMMENT '主键',
+    `id`           bigint unsigned                                               NOT NULL AUTO_INCREMENT COMMENT '主键',
     `config_name`  varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '参数名称',
     `config_key`   varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '参数键名',
     `config_value` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci          DEFAULT '' COMMENT '参数键值',
-    `config_type`  int UNSIGNED                                                  NOT NULL DEFAULT '0' COMMENT '系统内置（0否,1是 ）',
+    `config_type`  int unsigned                                                  NOT NULL DEFAULT '0' COMMENT '系统内置（0否,1是 ）',
     `create_time`  timestamp                                                     NULL     DEFAULT NULL COMMENT '创建时间',
     `update_time`  timestamp                                                     NULL     DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
     `remark`       varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci          DEFAULT NULL COMMENT '备注',
     PRIMARY KEY (`id`) USING BTREE,
     UNIQUE KEY `index_config_key` (`config_key`) USING BTREE COMMENT 'config_key唯一键值索引'
 ) ENGINE = InnoDB
-  AUTO_INCREMENT = 3
+  AUTO_INCREMENT = 5
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_general_ci
   ROW_FORMAT = DYNAMIC COMMENT ='参数配置';
@@ -46,6 +46,9 @@ BEGIN;
 INSERT INTO `easy_config` (`id`, `config_name`, `config_key`, `config_value`, `config_type`, `create_time`,
                            `update_time`, `remark`)
 VALUES (1, '启用注册功能', 'enable_register', '0', 1, '2022-07-05 17:24:02', '2022-07-05 17:26:34', '');
+INSERT INTO `easy_config` (`id`, `config_name`, `config_key`, `config_value`, `config_type`, `create_time`,
+                           `update_time`, `remark`)
+VALUES (4, '登录超时时间', 'login_timeout', '280', 1, '2022-07-06 11:25:05', '2022-07-06 11:25:05', '登录超时时间/分钟');
 COMMIT;
 
 -- ----------------------------
@@ -54,16 +57,16 @@ COMMIT;
 DROP TABLE IF EXISTS `easy_dict_data`;
 CREATE TABLE `easy_dict_data`
 (
-    `id`           bigint UNSIGNED                                               NOT NULL AUTO_INCREMENT COMMENT '字典编码',
-    `dict_type_id` bigint UNSIGNED                                               NOT NULL COMMENT '字典类型ID',
+    `id`           bigint unsigned                                               NOT NULL AUTO_INCREMENT COMMENT '字典编码',
+    `dict_type_id` bigint unsigned                                               NOT NULL COMMENT '字典类型ID',
     `dict_type`    varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '字典类型',
-    `dict_sort`    int UNSIGNED                                                           DEFAULT '0' COMMENT '字典排序',
+    `dict_sort`    int unsigned                                                           DEFAULT '0' COMMENT '字典排序',
     `dict_label`   varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci          DEFAULT '' COMMENT '字典标签',
     `dict_value`   varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '字典键值',
     `css_class`    varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci          DEFAULT NULL COMMENT '样式属性（其他样式扩展）',
     `list_class`   varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci          DEFAULT NULL COMMENT '表格回显样式',
-    `is_default`   int UNSIGNED                                                  NOT NULL DEFAULT '0' COMMENT '是否默认(0否,1-是)',
-    `status`       int UNSIGNED                                                  NOT NULL DEFAULT '0' COMMENT '状态(0-正常,1-停用)',
+    `is_default`   int unsigned                                                  NOT NULL DEFAULT '0' COMMENT '是否默认(0否,1-是)',
+    `status`       int unsigned                                                  NOT NULL DEFAULT '0' COMMENT '状态(0-正常,1-停用)',
     `create_time`  timestamp                                                     NULL     DEFAULT NULL COMMENT '创建时间',
     `update_time`  timestamp                                                     NULL     DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
     `remark`       varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci          DEFAULT NULL COMMENT '备注',
@@ -87,10 +90,10 @@ COMMIT;
 DROP TABLE IF EXISTS `easy_dict_type`;
 CREATE TABLE `easy_dict_type`
 (
-    `id`          bigint UNSIGNED                                               NOT NULL AUTO_INCREMENT COMMENT '字典主键',
+    `id`          bigint unsigned                                               NOT NULL AUTO_INCREMENT COMMENT '字典主键',
     `dict_name`   varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '字典名称',
     `dict_type`   varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '字典类型',
-    `status`      int UNSIGNED                                                  NOT NULL DEFAULT '0' COMMENT '状态（0正常 1停用）',
+    `status`      int unsigned                                                  NOT NULL DEFAULT '0' COMMENT '状态（0正常 1停用）',
     `create_time` timestamp                                                     NULL     DEFAULT NULL COMMENT '创建时间',
     `update_time` timestamp                                                     NULL     DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
     `remark`      varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci          DEFAULT NULL COMMENT '备注',
@@ -113,16 +116,16 @@ COMMIT;
 DROP TABLE IF EXISTS `easy_menu`;
 CREATE TABLE `easy_menu`
 (
-    `id`          bigint UNSIGNED                                              NOT NULL AUTO_INCREMENT COMMENT '主键',
+    `id`          bigint unsigned                                              NOT NULL AUTO_INCREMENT COMMENT '主键',
     `menu_name`   varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '菜单名称',
-    `parent_id`   bigint UNSIGNED                                                   DEFAULT '0' COMMENT '父菜单ID',
+    `parent_id`   bigint unsigned                                                   DEFAULT '0' COMMENT '父菜单ID',
     `order_num`   int                                                               DEFAULT '0' COMMENT '显示顺序',
     `path`        varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci     DEFAULT '' COMMENT '路由地址',
     `component`   varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci     DEFAULT NULL COMMENT '组件路径',
-    `is_frame`    int UNSIGNED                                                      DEFAULT '0' COMMENT '是否为外链(0-否,1-是)\n',
-    `menu_type`   char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci          DEFAULT '' COMMENT '菜单类型（M目录 C菜单 F按钮）',
-    `visible`     int                                                               DEFAULT '0' COMMENT '菜单状态（0显示 1隐藏）',
-    `status`      int                                                               DEFAULT '0' COMMENT '菜单状态（0正常 1停用）',
+    `is_frame`    int unsigned                                                      DEFAULT '0' COMMENT '是否为外链(0-否;1-是)\n',
+    `menu_type`   int unsigned                                                      DEFAULT NULL COMMENT '菜单类型（0目录;1页面;2按钮）',
+    `show`        int                                                               DEFAULT '0' COMMENT '菜单是否显示（0隐藏;1显示）',
+    `enable`      int unsigned                                                      DEFAULT '1' COMMENT '菜单是否启用（0停用;1正常 ）',
     `perms`       varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci     DEFAULT NULL COMMENT '权限标识',
     `icon`        varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci     DEFAULT '#' COMMENT '菜单图标',
     `create_time` timestamp                                                    NULL DEFAULT NULL COMMENT '创建时间',
@@ -145,11 +148,11 @@ COMMIT;
 DROP TABLE IF EXISTS `easy_role`;
 CREATE TABLE `easy_role`
 (
-    `id`          bigint UNSIGNED                                               NOT NULL AUTO_INCREMENT COMMENT '主键',
+    `id`          bigint unsigned                                               NOT NULL AUTO_INCREMENT COMMENT '主键',
     `role_name`   varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci  NOT NULL COMMENT '角色名字',
     `role_key`    varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '角色key',
-    `role_sort`   int UNSIGNED                                                  NOT NULL DEFAULT '0' COMMENT '显示顺序',
-    `status`      int UNSIGNED                                                  NOT NULL DEFAULT '0' COMMENT '角色状态（0正常 1停用）',
+    `role_sort`   int unsigned                                                  NOT NULL DEFAULT '0' COMMENT '显示顺序',
+    `status`      int unsigned                                                  NOT NULL DEFAULT '0' COMMENT '角色状态（0正常 1停用）',
     `create_time` timestamp                                                     NULL     DEFAULT NULL COMMENT '创建时间',
     `update_time` timestamp                                                     NULL     DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
     `remark`      varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci          DEFAULT NULL COMMENT '备注',
@@ -171,9 +174,9 @@ COMMIT;
 DROP TABLE IF EXISTS `easy_role_menu`;
 CREATE TABLE `easy_role_menu`
 (
-    `id`          bigint UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键',
-    `role_id`     bigint UNSIGNED NOT NULL COMMENT '角色ID',
-    `menu_id`     bigint UNSIGNED NOT NULL COMMENT '菜单ID',
+    `id`          bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
+    `role_id`     bigint unsigned NOT NULL COMMENT '角色ID',
+    `menu_id`     bigint unsigned NOT NULL COMMENT '菜单ID',
     `create_time` timestamp       NULL                                          DEFAULT NULL COMMENT '创建时间',
     `update_time` timestamp       NULL                                          DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
     `remark`      varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '备注',
@@ -198,14 +201,14 @@ COMMIT;
 DROP TABLE IF EXISTS `easy_user`;
 CREATE TABLE `easy_user`
 (
-    `id`             bigint UNSIGNED                                               NOT NULL AUTO_INCREMENT COMMENT '主键',
+    `id`             bigint unsigned                                               NOT NULL AUTO_INCREMENT COMMENT '主键',
     `user_key`       varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci  NOT NULL COMMENT '用户特殊编码',
     `username`       varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci  NOT NULL COMMENT '账号',
     `phone`          varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci           DEFAULT NULL COMMENT '手机号',
     `email`          varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci          DEFAULT NULL COMMENT '邮箱',
     `password`       varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '密码',
     `register_time`  timestamp                                                     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '注册时间',
-    `account_status` int UNSIGNED                                                  NOT NULL COMMENT '账号状态',
+    `account_status` int unsigned                                                  NOT NULL COMMENT '账号状态',
     `create_time`    timestamp                                                     NULL     DEFAULT NULL COMMENT '创建时间',
     `update_time`    timestamp                                                     NULL     DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
     `remark`         varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci          DEFAULT NULL COMMENT '备注',
@@ -235,8 +238,8 @@ COMMIT;
 DROP TABLE IF EXISTS `easy_user_info`;
 CREATE TABLE `easy_user_info`
 (
-    `id`          bigint UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键',
-    `user_id`     bigint UNSIGNED NOT NULL COMMENT '用户id',
+    `id`          bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
+    `user_id`     bigint unsigned NOT NULL COMMENT '用户id',
     `nickname`    varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '昵称',
     `create_time` timestamp       NULL                                          DEFAULT NULL COMMENT '创建时间',
     `update_time` timestamp       NULL                                          DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
@@ -264,9 +267,9 @@ COMMIT;
 DROP TABLE IF EXISTS `easy_user_role`;
 CREATE TABLE `easy_user_role`
 (
-    `id`          bigint UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键',
-    `user_id`     bigint UNSIGNED NOT NULL COMMENT '用户ID',
-    `role_id`     bigint UNSIGNED NOT NULL COMMENT '角色ID',
+    `id`          bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
+    `user_id`     bigint unsigned NOT NULL COMMENT '用户ID',
+    `role_id`     bigint unsigned NOT NULL COMMENT '角色ID',
     `create_time` timestamp       NULL                                          DEFAULT NULL COMMENT '创建时间',
     `update_time` timestamp       NULL                                          DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
     `remark`      varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '备注',
